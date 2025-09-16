5546c909b8bf15d85020144b114c59eb793c5c2c
syedsmuzakkir <syedsmuzakkir46@gmail.com>
2025-09-16 21:30:13 +0530

    base url changed

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://taxation-backend-test.onrender.com';
+// export const BASE_URL = 'https://taxation-backend-test.onrender.com';
 
 // export const BASE_URL = 'https://2d7c66834df6915df2cf25889998307d.serveo.net'
 
-// export const BASE_URL = 'https://api.invertio.us';
+export const BASE_URL = 'https://api.invertio.us';
